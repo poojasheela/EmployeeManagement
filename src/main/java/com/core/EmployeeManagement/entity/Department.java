0abd47b32c6c811baa0e9df18ce7962868a6acdc
poojasheela <pooja_sheela@mystiquex.com>
2025-07-22 17:57:58 +0530

    refactor

--- src/main/java/com/core/EmployeeManagement/entity/Department.java
+++ src/main/java/com/core/EmployeeManagement/entity/Department.java
@@ -5,33 +5,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Department {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
 
-    public Department(String name) {
-        this.name = name;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
